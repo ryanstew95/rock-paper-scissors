02b9de2d48504b786fdd43ed1b4d404d9d424e9a
Ryan Stewart <ryanstew17@gmail.com>
2023-11-21 19:59:21 -0400

    function for getting result

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id
   userChoiceDisplay.innerHTML = userChoice
   generateComputerChoice()
+  getResult()
 }));
 
 function generateComputerChoice() {
@@ -25,4 +27,29 @@ function generateComputerChoice() {
     computerChoice = 'paper'
   }
   computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = 'its a draw!'
+  }
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = 'you win!'
+  }
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = 'you lose!'
+  }
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = 'you win!'
+  }
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = 'you lose!'
+  }
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = 'you win!'
+  }
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    result = 'you lose!'
+  }
+  resultDisplay.innerHTML = result
 }
