93153c4c313c09455590598aabcb44ef0eee6bb0
Ryan Stewart <ryanstew17@gmail.com>
2023-11-21 19:51:32 -0400

    function for computer choice

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ const userChoiceDisplay = document.getElementById('your-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id
   userChoiceDisplay.innerHTML = userChoice
   generateComputerChoice()
 }));
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1
+
+  if (randomNumber === 1) {
+    computerChoice = 'rock'
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'scissors'
+  }
+
+  if (randomNumber === 3) {
+    computerChoice = 'paper'
+  }
+  computerChoiceDisplay.innerHTML = computerChoice
+}
