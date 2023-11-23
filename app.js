01488227288120d1430fccc8ab06b31e19e5e331
Ryan Stewart <ryanstew17@gmail.com>
2023-11-23 13:32:18 -0400

    added images to game

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('your-choice');
 const resultDisplay = document.getElementById('result');
+const userChoiceImg = document.getElementById('user-choice-img');
+const computerChoiceImg = document.getElementById('computer-choice-img');
 const possibleChoices = document.querySelectorAll('button');
 const resetButton = document.getElementById('reset');
 
-let userChoice
-let computerChoice
-let result
+let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice
-  generateComputerChoice()
-  getResult()
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = userChoice;
+  userChoiceImg.src = `img/user/${userChoice}.png`;
+  generateComputerChoice();
+  getResult();
 }));
 
 resetButton.addEventListener('click', () => {
   userChoiceDisplay.innerHTML = '';
   computerChoiceDisplay.innerHTML = '';
   resultDisplay.innerHTML = '';
+  userChoiceImg.src = '';
+  computerChoiceImg.src = '';
   userChoice = null;
   computerChoice = null;
   result = null;
 });
 
 function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
 
   if (randomNumber === 1) {
-    computerChoice = 'rock'
+    computerChoice = 'rock';
   }
   if (randomNumber === 2) {
-    computerChoice = 'scissors'
+    computerChoice = 'scissors';
   }
 
   if (randomNumber === 3) {
-    computerChoice = 'paper'
+    computerChoice = 'paper';
   }
-  computerChoiceDisplay.innerHTML = computerChoice
+  computerChoiceDisplay.innerHTML = computerChoice;
+  computerChoiceImg.src = `img/computer/${computerChoice}.png`;
 }
 
 function getResult() {
